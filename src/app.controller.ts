93559b957278d2afe4d499a1534fe59cdb0f0457
David-Balazs-Kovacs <kdbazsi@gmail.com>
2024-02-01 16:01:49 +0100

    commit

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,36 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
 import { AppService } from './app.service';
+import type {Response} from 'express';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('/hello')
   getHello(): string {
     return this.appService.getHello();
   }
 }
+
+@Controller()
+export class PageController {
+  constructor(private readonly appService: AppService) {}
+
+@Get('/html')
+getHTML(@Res() res: Response) {
+  res.sendFile('/index.html',  {root: __dirname + '/public'});
+}
+}
+
+@Controller()
+export class AdminController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get('/titkos')
+  getSecret(): string {
+    return 'jelszó: 1234';
+  }
+}
+// http://localhost:3000/api/hello
+// http://localhost:3000/page/html  
+
