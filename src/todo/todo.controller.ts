909cddd3c71648d5476a84c9e3fe799a0eccdbc8
David-Balazs-Kovacs <kdbazsi@gmail.com>
2024-02-16 15:59:25 +0100

    lopom a napot...nagyon!

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,20 +1,20 @@
 import {
-  BadRequestException,
   Body,
-  Controller, Delete,
+  Controller,
+  Delete,
   Get,
   Param,
-  Post, Put,
+  Post,
+  Put,
   Query,
-  Res
+  Res,
 } from '@nestjs/common';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { TodoService } from './todo.service';
 import type { Response } from 'express';
 import type { CreateToDoInput } from './todo.service';
-import { TodoModule } from "./todo.module";
-import { TodoModel } from "./todo.model";
-import { QueryTodoInputDto } from "./queryTodoInput.dto";
+import { TodoModel } from './todo.model';
+import { QueryTodoInputDto } from './queryTodoInput.dto';
 
 @Controller('todo')
 export class TodoController {
@@ -31,13 +31,16 @@ export class TodoController {
   }
 
   @Put('/todos/:param')
-  updateTodo(@Param('param') param: string, @Body() responseBody: CreateToDoInput){
+  updateTodo(
+    @Param('param') param: string,
+    @Body() responseBody: CreateToDoInput,
+  ) {
     const paramNum: number = Number(param);
     return this.TodoService.updateTodo(paramNum, responseBody);
   }
 
   @Delete('/todos/:param')
-  deleteTodo(@Param('param') id: string){
+  deleteTodo(@Param('param') id: string) {
     const newId: number = Number(id);
     return this.TodoService.deleteTodo(newId);
   }
@@ -50,8 +53,8 @@ export class TodoController {
   }
 
   @Get('/todos-html')
-  listTodos(@Res() res: Response){
-    res.sendFile('todos.html', {root: 'public'})
+  listTodos(@Res() res: Response) {
+    res.sendFile('todos.html', { root: 'public' });
     return this.TodoService.listTodos();
   }
 }
